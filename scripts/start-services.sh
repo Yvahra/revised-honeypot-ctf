6ccb8685a9836c704cefaee03987f9cafff75bc5
Yvahra <79484088+Yvahra@users.noreply.github.com>
2025-06-12 10:24:28 +0200

    Create start-services.sh

---
+++ scripts/start-services.sh
@@ -0,0 +1,9 @@
+import os
+
+# Start containers (no port mappings to host)
+os.system("docker build -t 1 /app/ssh/")
+os.system("docker run -d --name 1_c -h \"service1\" --rm 1")
+#docker run -d --name "$HONEYPOT1_NAME" -h "$HONEYPOT1_NAME" --rm your-honeypot1-image:latest
+#docker run -d --name "$HONEYPOT2_NAME" -h "$HONEYPOT2_NAME" --rm your-honeypot2-image:latest
+#docker run -d --name "$HONEYPOT3_NAME" -h "$HONEYPOT3_NAME" --rm your-honeypot3-image:latest
+
